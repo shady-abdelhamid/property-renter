8ea27b5ac957d6dccae7d0bbf015c9a26bc4c415
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-11-10 12:13:06 +0200

    Use tabs for child modules inside places

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,8 +5,52 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover', children: [
+          {
+            path: '',
+            loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./discover/place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)
+          },
+        ],
+      },
+      {
+        path: 'offers', children: [
+          {
+            path: '',
+            loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: () => import('./offers/new-offer/new-offer.module').then(m => m.NewOfferPageModule)
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () => import('./offers/edit-offer/edit-offer.module').then(m => m.EditOfferPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then(m => m.OfferBookingsPageModule)
+          },
+        ],
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full'
+      },
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
 ];
 
@@ -14,4 +58,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PlacesPageRoutingModule {}
+export class PlacesPageRoutingModule { }
